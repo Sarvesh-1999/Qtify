7b14a6474d771abd784a58826e5c45c8a01fb4ae
Sarvesh <sarvesh.kumar.jsr@gmail.com>
2025-07-25 19:49:11 +0530

    updates

--- src/components/albumCard/AlbumCard.jsx
+++ src/components/albumCard/AlbumCard.jsx
@@ -1,15 +1,15 @@
 const AlbumCard = ({ image, title, followers }) => {
   return (
-    <div  className="rounded-t-2xl ">
+        <div  className="rounded-t-2xl ">
       <div className="">
         <img src={image} alt="album image" className="rounded-t-2xl" />
-        <div className="flex p-3 rounded-b-2xl border bg-[var(--white)]">
-          <div className="bg-[var(--black)] rounded-2xl text-sm py-1 px-2">
+        <div className="flex p-3 rounded-b-2xl border bg-[var(--color-white)]">
+          <div className="bg-[var(--color-black)] rounded-2xl text-sm py-1 px-2">
             {followers}
           </div>
         </div>
 
-        <p className="bg-[var(--black)] text-[var(--white)] text-[14px] py-2">
+        <p className="bg-[var(--color-black)] text-[var(--color-white)] text-[14px] py-2">
           {title}
         </p>
       </div>
