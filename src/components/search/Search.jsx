180f9fa627c420f978b4f41f4c72634ac89e93a5
Sarvesh <sarvesh.kumar.jsr@gmail.com>
2025-07-25 20:48:55 +0530

    search updated

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,7 +3,7 @@ import SearchIcon from "../../assets/search-icon.svg"
 const Search = () => {
   return (
     <section id={style.searchWrapper}>
-      <input type="text" name="" id="" placeholder="search" />
+      <input type="text" name="" id="" placeholder="search a song" />
       <span><img src={SearchIcon} alt="" /></span>
     </section>
   )
